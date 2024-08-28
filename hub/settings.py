0e65f6065960b81b81e43fd990d7d1db76a8f230
chiagoziemng <chiagoziemjamesng@gmail.com>
2024-08-28 23:48:59 +0100

    Fest: redeploy db configuration

--- hub/settings.py
+++ hub/settings.py
@@ -103,11 +103,7 @@ WSGI_APPLICATION = 'hub.wsgi.application'
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/hub',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default='postgres://user:password@localhost:5432/hub')
 }
 
 # Password validation
@@ -145,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
@@ -154,6 +151,7 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Optionally, you can add these if you plan to collect static files in production
