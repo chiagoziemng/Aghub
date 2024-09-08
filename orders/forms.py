54e90f24b1e33080244a38118af4394e92b2267c
chiagoziemng <chiagoziemjamesng@gmail.com>
2024-09-08 15:12:56 +0100

    fest: codinated the shop experience from product page to thank you page and the main focus was the interface

--- orders/forms.py
+++ orders/forms.py
@@ -6,3 +6,8 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control required'})
